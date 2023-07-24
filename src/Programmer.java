058fd971e2352a486de42a06ca6b0d7cd2327a40
Sara <saracoppola996@gmail.com>
2023-07-24 19:05:36 +0200

    first commit

---
+++ src/Programmer.java
@@ -0,0 +1,17 @@
+public class Programmer {
+    String name = "Rosa";
+    int age = 18;
+    boolean wearGlasses = true;
+
+    void drinkCoffee(){
+        System.out.println("Espresso is the secret!");
+    }
+    void printDetails(){
+        System.out.println(name + " is a " + age +"-yo programmer");
+    }
+    void hasGlasses(){
+        System.out.println("Is " + name + " wearing glasses? " + wearGlasses);
+    }
+}
+
+
